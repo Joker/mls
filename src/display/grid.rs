f204e446e7c53ea707cdf5a7f591b2e97aaa7b36
Joker <nice.way@live.com>
2023-03-21 17:52:19 +0300

    without name_len

--- src/display/grid.rs
+++ src/display/grid.rs
@@ -3,39 +3,40 @@ use std::usize;
 use super::{spaces, INDENT};
 use crate::{termsize::terminal_size, File};
 
-fn width_sizes(names: &Vec<usize>, stack_size: &usize) -> (usize, Vec<usize>) {
-	let mut out = Vec::new();
+fn width_sizes(names: &Vec<File>, stack_size: &usize) -> (usize, Vec<usize>) {
+	let mut col_sizes = Vec::new();
 	let mut count = 0;
 	let mut maximum = 0;
 
 	for n in names {
 		if count == *stack_size {
-			out.push(maximum);
+			col_sizes.push(maximum);
 			maximum = 0;
 			count = 0;
 		}
-		maximum = if *n > maximum { *n } else { maximum };
+		maximum = if n.len > maximum { n.len } else { maximum };
 		count += 1;
 	}
 	if maximum > 0 {
-		out.push(maximum)
+		col_sizes.push(maximum)
 	}
 
-	(out.iter().sum::<usize>(), out)
+	(col_sizes.iter().sum::<usize>(), col_sizes)
 }
 
-fn grid_size(names: &Vec<usize>) -> (usize, Vec<usize>) {
+fn grid_size(names: &Vec<File>) -> (usize, Vec<usize>) {
 	let (mw, _) = terminal_size().unwrap();
 	let term_width = mw as usize;
 
-	let mut stack = names.len() / (term_width / names.iter().max().unwrap());
+	let mut stack =
+		names.len() / (term_width / names.iter().map(|f| f.len).max().unwrap_or(term_width / 2));
 	let mut column_vec = Vec::new();
 
 	loop {
-		let (sum, row_sizes) = width_sizes(names, &stack);
+		let (sum, col_sizes) = width_sizes(names, &stack);
 
 		if term_width >= sum && stack != 0 {
-			column_vec = row_sizes;
+			column_vec = col_sizes;
 			stack -= 1;
 		} else {
 			return (stack + 1, column_vec);
@@ -44,12 +45,7 @@ fn grid_size(names: &Vec<usize>) -> (usize, Vec<usize>) {
 }
 
 pub fn to_string(files: &Vec<File>) -> String {
-	let mut name_len = Vec::new();
-	for file in files {
-		name_len.push(file.name.chars().count() + INDENT);
-	}
-
-	let (stack, column_sizes) = grid_size(&name_len);
+	let (stack, column_sizes) = grid_size(files);
 
 	if stack == 1 {
 		return files
@@ -63,13 +59,13 @@ pub fn to_string(files: &Vec<File>) -> String {
 	str_vec.resize(stack, String::from(""));
 
 	for (i, nm) in files.iter().enumerate() {
-		let j = i % stack;
-		let k = i / stack;
+		let row_num = i % stack;
+		let col_num = i / stack;
 
-		str_vec[j].push_str(&nm.name);
+		str_vec[row_num].push_str(&nm.name);
 
-		if column_sizes[k] >= name_len[i] {
-			str_vec[j].push_str(&spaces(column_sizes[k] - name_len[i] + INDENT));
+		if column_sizes[col_num] >= files[i].len {
+			str_vec[row_num].push_str(&spaces(column_sizes[col_num] - files[i].len + INDENT));
 		}
 	}
 
