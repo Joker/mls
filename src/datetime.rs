e52a43f5dd1fb01661de6c8593a3aa2e74da15c4
Joker <nice.way@live.com>
2023-03-28 14:20:19 +0300

    new datetime

--- src/datetime.rs
+++ src/datetime.rs
@@ -1,94 +1,15 @@
-// https://www.geeksforgeeks.org/convert-unix-timestamp-to-dd-mm-yyyy-hhmmss-format/
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use crate::display::TIMEZONE;
 
-fn leap_year(year: i32) -> bool {
-	year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
-}
-
-fn current_year(seconds: u64) -> (i32, u64) {
-	let mut curr_year = 1970;
-	let mut days_till_now = seconds / (24 * 60 * 60);
-	loop {
-		match leap_year(curr_year) {
-			true => {
-				if days_till_now < 366 {
-					return (curr_year, days_till_now);
-				}
-				days_till_now -= 366;
-			}
-			false => {
-				if days_till_now < 365 {
-					return (curr_year, days_till_now);
-				}
-				days_till_now -= 365;
-			}
-		}
-		curr_year += 1;
-	}
-}
-
-fn date_month(leap_year: bool, curr_year_days: i64) -> (i64, usize) {
-	let mut extra_days: i64 = curr_year_days + 1;
-
-	let days_of_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-	let mut month = 0;
-	let mut i = 0;
-
-	match leap_year {
-		true => loop {
-			if i == 1 {
-				if extra_days - 29 <= 0 {
-					break;
-				}
-				month += 1;
-				extra_days -= 29;
-			} else {
-				if extra_days - days_of_month[i] <= 0 {
-					break;
-				}
-				month += 1;
-				extra_days -= days_of_month[i];
-			}
-			i += 1;
-		},
-		false => loop {
-			if extra_days - days_of_month[i] <= 0 {
-				break;
-			}
-			month += 1;
-			extra_days -= days_of_month[i];
-			i += 1;
-		},
-	};
-
-	if extra_days > 0 {
-		return (extra_days, month + 1);
-	}
-	if month == 2 && leap_year {
-		return (29, month);
-	}
-
-	return (days_of_month[month - 1], month);
-}
-
 pub fn date_time_fmt(unix_time: u64) -> String {
-	// pub fn date_time_fmt(sys_time: SystemTime) -> String {
 	let now = SystemTime::now()
 		.duration_since(UNIX_EPOCH)
 		.unwrap()
 		.as_secs()
 		+ TIMEZONE;
-	// let unix_time = sys_time.duration_since(UNIX_EPOCH).unwrap().as_secs() + TIMEZONE;
-	let remaind_time = unix_time % (24 * 60 * 60);
 
-	let (curr_year, curr_year_days) = current_year(unix_time);
-	let (date, month) = date_month(leap_year(curr_year), curr_year_days as i64);
-
-	let hours = remaind_time / 3600;
-	let minutes = (remaind_time % 3600) / 60;
-	// let seconds = (remaind_time % 3600) % 60;
+	let (date, month, year, hours, minutes, _) = seconds_to_datetime(unix_time);
 
 	let month_name = [
 		"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
@@ -96,11 +17,70 @@ pub fn date_time_fmt(unix_time: u64) -> String {
 	if unix_time > now - 15768000 {
 		return format!(
 			"{: >2} {} {:0>2}:{:0>2}",
-			date,
-			month_name[month - 1],
-			hours,
-			minutes,
+			date, month_name[month], hours, minutes,
 		);
 	}
-	format!("{: >2} {}  {}", date, month_name[month - 1], curr_year,)
+	format!("{: >2} {}  {}", date, month_name[month], year,)
+}
+
+// https://stackoverflow.com/questions/11188621/how-can-i-convert-seconds-since-the-epoch-to-hours-minutes-seconds-in-java/11197532#11197532
+fn seconds_to_datetime(unux_time: u64) -> (u64, usize, u64, u64, u64, u64) {
+	let days_since_jan1st = [
+		[0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365], // 365 days, non-leap
+		[0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366], // 366 days, leap
+	];
+	let mut sec = unux_time + 11644473600;
+
+	// let wday = (sec / 86400 + 1) % 7; // day of week
+
+	let q = sec / 12622780800; // 400*365.2425*24*3600
+	sec %= 12622780800;
+
+	let mut c = sec / 3155673600; // 100*(365+24/100)*24*3600
+	if c > 3 {
+		c = 3;
+	}
+	sec -= c * 3155673600;
+
+	let mut d = sec / 126230400; // 4*(365+1/4)*24*3600
+	if d > 24 {
+		d = 24;
+	}
+	sec -= d * 126230400;
+
+	let mut a = sec / 31536000; // 365*24*3600
+	if a > 3 {
+		a = 3;
+	}
+	sec -= a * 31536000;
+
+	// Calculate the year and find out if it's leap
+	let year = 1601 + q * 400 + c * 100 + d * 4 + a;
+	let leap = if year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) {
+		1
+	} else {
+		0
+	};
+
+	// Calculate the day of the year and the time
+	let yday = sec / 86400;
+	sec %= 86400;
+	let hour = sec / 3600;
+	sec %= 3600;
+	let min = sec / 60;
+	sec %= 60;
+
+	// Calculate the month
+	let mut mday = 1;
+	let mut month = 1;
+	for m in 1..13 {
+		month = m;
+		if yday < days_since_jan1st[leap][m] {
+			mday += yday - days_since_jan1st[leap][m - 1];
+			break;
+		}
+	}
+	// 	wday [0,6]   (day of week)
+	// 	yday [0,365] (day of year)
+	(mday, month - 1, year, hour, min, sec)
 }
