181485624d8913c83d4004fac7cb2fc5a585db31
Joker <nice.way@live.com>
2023-04-12 22:20:56 +0300

    hollow in a tree

--- src/display/tree.rs
+++ src/display/tree.rs
@@ -10,20 +10,9 @@ use crate::{
 pub static END: &str = "└──";
 pub static LEAF: &str = "├──";
 pub static TRUNK: &str = "│  ";
-pub static GROUND: &str = "   ";
+pub static HOLLOW: &str = "   ";
 
-pub fn trunc(width: usize, last_dir: usize) -> String {
-	if last_dir == 0 {
-		return (0..width).into_iter().map(|_| TRUNK).collect();
-	}
-	format!(
-		"{}{}",
-		(0..width).into_iter().map(|_| GROUND).collect::<String>(),
-		(0..(width - last_dir)).into_iter().map(|_| TRUNK).collect::<String>(),
-	)
-}
-
-pub fn list(path: &Path, fl: &Flags, w: &mut Width, lvl: usize, last_dir: usize) -> Vec<File> {
+pub fn list(path: &Path, fl: &Flags, w: &mut Width, lvl: usize, trunk: String) -> Vec<File> {
 	let mut out = Vec::new();
 	if fl.lvl <= lvl {
 		return out;
@@ -48,21 +37,21 @@ pub fn list(path: &Path, fl: &Flags, w: &mut Width, lvl: usize, last_dir: usize)
 		out.push(f);
 	}
 
-	let last = files.iter().last().unwrap();
+	let last_file = files.iter().last().unwrap();
 	files.iter().for_each(|f| {
-		let last_eq = std::ptr::eq(f, last);
+		let last = std::ptr::eq(f, last_file);
 		let mut fclone = f.clone();
-		fclone.name = format!(
-			"{WHITE}{}{} {}",
-			trunc(lvl, last_dir),
-			if last_eq { END } else { LEAF },
-			f.name
-		);
+		fclone.name = format!("{WHITE}{}{} {}", trunk, if last { END } else { LEAF }, f.name);
 		out.push(fclone);
 
 		if f.dir {
-			let ld = if last_eq { last_dir + 1 } else { last_dir };
-			let subt = list(path.join(Path::new(&f.sname)).as_path(), fl, w, lvl + 1, ld);
+			let subt = list(
+				path.join(Path::new(&f.sname)).as_path(),
+				fl,
+				w,
+				lvl + 1,
+				if last { format!("{}{}", trunk, HOLLOW) } else { format!("{}{}", trunk, TRUNK) },
+			);
 			out.extend(subt);
 		}
 	});
